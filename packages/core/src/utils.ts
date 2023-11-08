1088c087ae32b4ed1f1a03cb8caadc39087162b0
Thiendekaco <thienguo@gmail.com>
2023-11-08 15:32:04 +0700

    issues 3# Intergrate Subtrate Wallet Connect Wallets

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,5 +1,5 @@
 import bowser from 'bowser'
-
+import axios from 'axios';
 import type {
   Device,
   DeviceBrowser,
@@ -29,7 +29,16 @@ import {
   harmonyOneIcon,
   arbitrumIcon,
   baseIcon,
-  degenIcon
+  polkadotIcon,
+  crutsIcon,
+  acalaIcon,
+  astarNetworkIcon,
+  hydraxIcon,
+  kusamaIcon,
+  phalaNetworkIcon,
+  statemint,
+  turingNetworkIcon,
+  westendIcon
 } from './icons/index.js'
 
 import type {
@@ -61,7 +70,7 @@ export function getDevice(): Device | DeviceNotBrowser {
 }
 
 export const notNullish = <T>(value: T | null | undefined): value is T =>
-  value != null
+    value != null
 
 export function validEnsChain(chainId: ChainId): ChainId | null {
   // return L2s as Eth for ens resolution
@@ -73,6 +82,8 @@ export function validEnsChain(chainId: ChainId): ChainId | null {
     case '0xa4ba': // Arb Nova
     case '0x144': // zksync
       return '0x1'
+    case '0x5': // Goerli
+      return chainId
     case '0xaa36a7': // Sepolia
       return chainId
     default:
@@ -90,8 +101,8 @@ export function shortenAddress(add: string): string {
 
 export function shortenDomain(domain: string): string {
   return domain.length > 11
-    ? `${domain.slice(0, 4)}…${domain.slice(-6)}`
-    : domain
+      ? `${domain.slice(0, 4)}…${domain.slice(-6)}`
+      : domain
 }
 
 export async function copyWalletAddress(text: string): Promise<void> {
@@ -104,7 +115,7 @@ export async function copyWalletAddress(text: string): Promise<void> {
 }
 
 export const toHexString = (val: number | string): string =>
-  typeof val === 'number' ? `0x${val.toString(16)}` : val
+    typeof val === 'number' ? `0x${val.toString(16)}` : val
 
 export function chainIdToHex(chains: (Chain | ChainWithDecimalId)[]): Chain[] {
   return chains.map(({ id, ...rest }) => {
@@ -119,12 +130,16 @@ export function gweiToWeiHex(gwei: number): string {
 
 export const chainIdToLabel: Record<string, string> = {
   '0x1': 'Ethereum',
+  '0x3': 'Ropsten',
+  '0x4': 'Rinkeby',
+  '0x5': 'Goerli',
   '0xaa36a7': 'Sepolia',
+  '0x2a': 'Kovan',
   '0x38': 'Binance',
   '0x89': 'Polygon',
   '0xfa': 'Fantom',
-  '0xa': 'OP Mainnet',
-  '0x45': 'OP Kovan',
+  '0xa': 'Optimism',
+  '0x45': 'Optimism Kovan',
   '0xa86a': 'Avalanche',
   '0xa4ec': 'Celo',
   '0x2105': 'Base',
@@ -133,88 +148,154 @@ export const chainIdToLabel: Record<string, string> = {
   '0x63564C40': 'Harmony One',
   '0xa4b1': 'Arbitrum One',
   '0xa4ba': 'Arbitrum Nova',
-  '0x27bc86aa': 'Degen'
+  '91b171bb158e2d3848fa23a9f1c25182' : 'Polkadot',
+  'acala' : 'Acala',
+  '9eb76c5184c4ab8679d2d5d819fdf90b' : 'Astar Network',
+  'd4c0c08ca49dc7c680c3dac71a7c0703' : 'Crust',
+  'afdc188f45c71dacbaa0b62e16a91f72' : 'Hydradx',
+  'b0a8d493285c2df73290dfb7e61f870f' : 'Kusama',
+  '1bb969d85965e4bb5a651abbedf21a54' : 'Phala Network',
+  '68d56f15f85d3136970ec16946040bc1' : 'Statemint',
+  '0f62b701fb12d02237a33b84818c11f6' : 'Turing Network',
+  'e143f23803ac50e8f6f8e62695d1ce9e' : 'Westend'
 }
 
 export const networkToChainId: Record<string, ChainId> = {
   main: '0x1',
-  sepolia: '0xaa36a7',
+  ropsten: '0x3',
+  rinkeby: '0x4',
+  goerli: '0x5',
+  kovan: '0x2a',
   xdai: '0x64',
   'bsc-main': '0x38',
   'matic-main': '0x89',
   'fantom-main': '0xfa',
   'matic-mumbai': '0x80001',
-  degen: '0x27bc86aa'
 }
+
+
+
 
 export const chainStyles: Record<string, ChainStyle> = {
   '0x1': {
-    icon: ethereumIcon,
-    color: '#627EEA'
+    icon : ethereumIcon,
+    color : '#627EEA'
+  },
+  '0x3': {
+    icon : ethereumIcon,
+    color : '#627EEA'
+  },
+  '0x4': {
+    icon : ethereumIcon,
+    color : '#627EEA'
+  },
+  '0x5': {
+    icon : ethereumIcon,
+    color : '#627EEA'
+  },
+  '0x2a': {
+    icon : ethereumIcon,
+    color : '#627EEA'
   },
   '0xaa36a7': {
-    icon: ethereumIcon,
-    color: '#627EEA'
+    icon : ethereumIcon,
+    color : '#627EEA'
   },
   '0x38': {
-    icon: binanceIcon,
-    color: '#F3BA2F'
+    icon : binanceIcon,
+    color : '#F3BA2F'
   },
   '0x89': {
-    icon: polygonIcon,
-    color: '#8247E5'
+    icon : polygonIcon,
+    color : '#8247E5'
   },
   '0xfa': {
-    icon: fantomIcon,
-    color: '#1969FF'
+    icon : fantomIcon,
+    color : '#1969FF'
   },
   '0xa': {
-    icon: optimismIcon,
-    color: '#FF0420'
+    icon : optimismIcon,
+    color : '#FF0420'
   },
   '0x45': {
-    icon: optimismIcon,
-    color: '#FF0420'
+    icon : optimismIcon,
+    color : '#FF0420'
   },
   '0xa86a': {
-    icon: avalancheIcon,
-    color: '#E84142'
+    icon : avalancheIcon,
+    color : '#E84142'
   },
   '0xa4ec': {
-    icon: celoIcon,
-    color: '#FBCC5C'
+    icon : celoIcon,
+    color : '#FBCC5C'
   },
   '0x64': {
-    icon: gnosisIcon,
-    color: '#04795B'
+    icon : gnosisIcon,
+    color : '#04795B'
   },
   '0x63564C40': {
-    icon: harmonyOneIcon,
-    color: '#ffffff'
+    icon : harmonyOneIcon,
+    color : '#ffffff'
   },
-  '0xa4b1': {
+  '0xa4b1':  {
     icon: arbitrumIcon,
     color: '#33394B'
   },
   '0xa4ba': {
-    icon: arbitrumIcon,
-    color: '#33394B'
+    icon : arbitrumIcon,
+    color : '#33394B'
   },
   '0x2105': {
-    icon: baseIcon,
-    color: '#0259F9'
+    icon : baseIcon,
+    color : '#0259F9'
   },
   '0x14a33': {
-    icon: baseIcon,
-    color: '#0259F9'
+    icon : baseIcon,
+    color : '#0259F9'
   },
   '0x80001': {
-    icon: polygonIcon,
-    color: '#8247E5'
+    icon : polygonIcon,
+    color : '#8247E5'
   },
-  '0x27bc86aa': {
-    icon: degenIcon,
-    color: '#a36dfe'
+  '91b171bb158e2d3848fa23a9f1c25182' : {
+    icon : polkadotIcon,
+    color : '#ffffff'
+  },
+  'acala' : {
+    icon : acalaIcon,
+    color : '#ffffff'
+  },
+  '9eb76c5184c4ab8679d2d5d819fdf90b' : {
+    icon : astarNetworkIcon,
+    color : '#ffffff'
+  },
+  'd4c0c08ca49dc7c680c3dac71a7c0703' : {
+    icon: crutsIcon,
+    color: '#000000'
+  },
+  'afdc188f45c71dacbaa0b62e16a91f72' : {
+    icon : hydraxIcon,
+    color : '#f453a1'
+  },
+  'b0a8d493285c2df73290dfb7e61f870f' : {
+    icon : kusamaIcon,
+    color: '#000000'
+  },
+  '1bb969d85965e4bb5a651abbedf21a54' : {
+    icon : phalaNetworkIcon,
+    color: '#000000'
+  },
+  '68d56f15f85d3136970ec16946040bc1' : {
+    icon : statemint,
+    color : '#000000'
+  },
+  '0f62b701fb12d02237a33b84818c11f6' : {
+    icon : turingNetworkIcon,
+    color: '#ffffff'
+  },
+  'e143f23803ac50e8f6f8e62695d1ce9e' : {
+    icon : westendIcon,
+    color: '#ffffff'
   }
 }
 
@@ -225,19 +306,19 @@ export function getDefaultChainStyles(chainId: string): ChainStyle | undefined {
 }
 
 export function connectedToValidAppChain(
-  walletConnectedChain: ConnectedChain,
-  chains: Chain[]
+    walletConnectedChain: ConnectedChain,
+    chains: Chain[]
 ): boolean {
   return !!chains.find(
-    ({ id, namespace }) =>
-      id === walletConnectedChain.id &&
-      namespace === walletConnectedChain.namespace
+      ({ id, namespace }) =>
+          id === walletConnectedChain.id &&
+          namespace === walletConnectedChain.namespace
   )
 }
 
 export function initializeWalletModules(
-  modules: WalletInit[],
-  device: Device
+    modules: WalletInit[],
+    device: Device
 ): WalletModule[] {
   return modules.reduce((acc, walletInit) => {
     const initialized = walletInit({ device })
@@ -276,7 +357,7 @@ export const defaultNotifyEventStyles: Record<string, NotifyEventStyles> = {
 }
 
 export const wait = (time: number): Promise<void> =>
-  new Promise(resolve => setTimeout(resolve, time))
+    new Promise(resolve => setTimeout(resolve, time))
 
 export function getLocalStore(key: string): string | null {
   try {
@@ -301,4 +382,25 @@ export function delLocalStore(key: string): void {
   } catch (error) {
     return
   }
+}
+
+export const baseURL = 'https://*/api/scan/account/tokens';
+
+export type dataTypeFetch  = {
+  address : string
+}
+export interface  clientAxiosProps {
+  url : string,
+  data: dataTypeFetch
+};
+
+
+export async function getBalanceSubstrate({ url, data } : clientAxiosProps){
+  if(url === '') return
+  const urlResult = baseURL.replace('*', url);
+  return await axios.post(urlResult, data)
+      .then( data_ => {
+        return data_.data.data ? data_.data.data.native[0].balance : '0';
+      })
+
 }
